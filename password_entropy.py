5f64d5e40785574b462a8dec2cfeb9f1daeff780
CyberWarr10r <64969369+L3arnH0wT0H4ck@users.noreply.github.com>
2023-01-02 13:45:36 -0800

    Update password_entropy.py

--- password_entropy.py
+++ password_entropy.py
@@ -1,7 +1,7 @@
 import math
 import string
 
-def get_entropie(user_password_length: int, password_characters_occurences_dict: dict) -> float:
+def get_entropy(user_password_length: int, password_characters_occurences_dict: dict) -> float:
     password_characters_length = 0
     for key, value in password_characters_occurences_dict.items():
         if value:
@@ -16,12 +16,12 @@ def get_entropie(user_password_length: int, password_characters_occurences_dict:
     password_space = password_characters_length ** user_password_length
     return math.log2(password_space)
 
-def password_level_sensibility(entropie: float) -> str:
-    if entropie < 66:
+def password_level_sensibility(entropy: float) -> str:
+    if entropy < 66:
         return "[red3 b][-][/red3 b] Password strength: [red3 b]very weak[/red3 b]"
-    elif entropie < 85:
+    elif entropy < 85:
         return "[red b][-][/red b] Password strength: [red3 b]weak[/red3 b]"
-    elif entropie < 100:
+    elif entropy < 100:
         return "[orange_red1 b][±][orange_red1 b] Password strength: [orange_red1 b]moderately good[/orange_red1 b]"
     else:
         return "[green b][+][/green b] Password strength:  [green b]good[/green b]"
