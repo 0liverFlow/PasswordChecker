3ccdfa9bf5a677d06031301dbcc480596f8ad663
Olivier Konaté <64969369+0liverFlow@users.noreply.github.com>
2023-04-24 11:47:28 -0700

    Update password_characters_occurences.py

--- password_characters_occurences.py
+++ password_characters_occurences.py
@@ -2,27 +2,16 @@ import string
 
 def get_password_characters_occurences(password: str) -> tuple:
     """
-    |───────────────────────────────────────────────────────────────────────────────────|
-    | This module is composed of 1 function namely :                                    |
-    |                                                                                   |
-    ==> get_password_characters_occurences(password)                                    |
-    |   This function only takes one parameter which is the user password and           |
-    |   return a dictionary and a list.                                                 |
-    |-- password represents the user password.                                          |
-    |   Indeed, the function will try to determine how many numbers, alphabetical       |
-    |   and special characters the password contains and                                |
-    |   it will store them in a dictionary : password_characters_occurences_dict.       |
-    |                                                                                   |
-    |   alphanumerical_characters_list is just a matrice which contains lists that      |
-    |   contain values of each set of characters of the same type.                      |
-    |   password_characters_occurences_dict, alphanumerical_characters_list will be     |
-    |   returned by the function and these 2 values.                                    |                                    
-    |   will be useful for getting the password characteristics module.                 |
-    |___________________________________________________________________________________|
+    This function takes one parameter which is the user's password and return a dictionary and a list.
+    Indeed, the function will try to determine how many numbers, alphabetical and special characters the password contains
+    and  it will store them in a dictionary : password_characters_occurences_dict.
+
+    :param password: This is the user's password.
     """
     alphabetical_characters = list()
     numerical_characters = list()
     special_characters_list = list()
+    string.punctuation = "!\#$%&'()*+,-./:;<= >?@[\]^_`{|}~"
     lower_characters_occurences = upper_characters_occurences = numbers_occurences = special_characters_occurences = 0
     for character in password:
         if character in string.ascii_lowercase:
