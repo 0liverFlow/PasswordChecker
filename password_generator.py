3520c34f5a3b2941bb3437f3ee4e883ad46a9785
Olivier Konaté <64969369+0liverFlow@users.noreply.github.com>
2023-04-24 11:48:51 -0700

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,28 +1,15 @@
-import random
 import string
+import secrets
+import pyperclip
 
-def password_generator(password_length):
+def password_generator(password_length: str) -> str:
     """
-    |───────────────────────────────────────────────────────────────────────────────────|
-    | This module is composed of 1 function namely :                                    |
-    |                                                                                   |
-    ==> password_generator()                                                            |
-    |   This function has no parameter and return nothing apart displaying the          |
-    |   generated password on the screen.                                               |
-    |   It will generate a password based on the lower, upper,                          |
-    |   special characters and numbers.                                                 |
-    |   Also, one thing which is important, it is gonna generate a                      |
-    |   password of 12 characters at least.                                             |
-    |   When a user enters a length less than 12 characters,                            |
-    |   the function will let them know about the minimum length authorized.            |
-    |___________________________________________________________________________________|
+    This function takes the password length and returns a generated secured password in the clipboard.
+    The minimum lenght of a generated password is 20 characters.
+
+    :param password_length: This is the password length
     """
     string.punctuation = "!\#$%&'()*+,-./:;<= >?@[\]^_`{|}~"
     characters_combination = string.ascii_lowercase + string.punctuation + string.ascii_uppercase + string.digits
-    combination_length = len(characters_combination)
-    combo = 0
-    while combo < 2000:
-        characters_combination = ''.join(random.sample(characters_combination, combination_length))
-        combo += 1
-    password =  characters_combination[:password_length]
-    return password
+    password = ''.join(secrets.choice(characters_combination) for combo in range(password_length))
+    pyperclip.copy(password)
