b16990b34248b2e6dbe25ad0213d63477388db5d
Olivier Konaté <64969369+0liverFlow@users.noreply.github.com>
2023-04-24 11:48:22 -0700

    Update password_entropy.py

--- password_entropy.py
+++ password_entropy.py
@@ -2,6 +2,14 @@ import math
 import string
 
 def get_entropy(user_password_length: int, password_characters_occurences_dict: dict) -> float:
+    """"
+    This function takes 2 parameters and return the password entropy.
+    The entropy is calculated based on ANSSI's recommendations.
+    https://www.ssi.gouv.fr/administration/precautions-elementaires/calculer-la-force-dun-mot-de-passe/
+
+    :param user_password_length: As the name implies, this is the user's password length
+    :param password_characters_occurences_dict: This is a dictionary representing every type of character (upper case, lower case, number, special characters) type with their respective number of occurences.
+    """
     password_characters_length = 0
     for key, value in password_characters_occurences_dict.items():
         if value:
@@ -17,11 +25,13 @@ def get_entropy(user_password_length: int, password_characters_occurences_dict: 
     return math.log2(password_space)
 
 def password_level_sensibility(entropy: float) -> str:
-    if entropy < 66:
-        return "[red3 b][-][/red3 b] Password strength: [red3 b]very weak[/red3 b]"
-    elif entropy < 85:
-        return "[red b][-][/red b] Password strength: [red3 b]weak[/red3 b]"
+    if entropy < 64:
+        return "[red3 b][-][/red3 b] Password entropy: [red3 b]Very weak[/red3 b]"
+    elif entropy < 80:
+        return "[red b][-][/red b] Password entropy: [red3 b]Weak[/red3 b]"
     elif entropy < 100:
-        return "[orange_red1 b][±][orange_red1 b] Password strength: [orange_red1 b]moderately good[/orange_red1 b]"
+        return "[orange_red1 b][±][orange_red1 b] Password entropy: [orange_red1 b]Moderately good[/orange_red1 b]"
+    elif entropy < 130:
+        return "[green b][+][/green b] Password entropy:  [green b]Good[/green b]"
     else:
-        return "[green b][+][/green b] Password strength:  [green b]good[/green b]"
+        return "[green b][+][/green b] Password entropy:  [green b]Excellent[/green b]"
