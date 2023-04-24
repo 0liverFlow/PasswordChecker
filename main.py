ae7d61ee26fc1e6f1ae7d528c937f7dcbfaea64f
Olivier Konaté <64969369+0liverFlow@users.noreply.github.com>
2023-04-24 10:45:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,46 @@
 from getpass import getpass
+import sys
+
+from rich import print as printc
+
 from password_characteristics import check_character_occurences_in_password, check_password_characters_mixture
 from password_characters_occurences import get_password_characters_occurences
 from password_generator import password_generator
 from test_password import determine_elapsed_time, format_time, crack_password
 from menu import password_checker_menu
 from password_entropy import get_entropy, password_level_sensibility
-import time
-from rich.console import Console
-console = Console()
-printc = console.print
+from have_i_been_pwned_check import check_haveibeenpwned_db
+
 
 if __name__ == '__main__':
     password_checker_menu()
-    user_password = getpass('\nEnter your passw0rd: ')
-    printc("\n[yellow b][!][/yellow b] Note: The rockyou dictionary will be used by default if no passwords dictionary is specified!\n")
-    user_response = input('Would you like to specify your own dictionary path[yay/Nay]? : ')
-    if user_response.lower() in ['yay', 'yes', 'y']:
-        user_response = True
-        passwords_dictionary_file_path = input('Thanks to specify your password dictionary file path (absolute path): ')  
-    else:
-        user_response = False
+    user_password = getpass('Enter your passw0rd: ')
+    # Checking Have I Been Pwned Database
+    printc("\n[cyan3 bold underline][*] Checking HaveIBeenPwned database[/cyan3 bold underline]\n")
+    printc(check_haveibeenpwned_db(user_password))
 
-    #Check user's password characters occurences and mixture
+    # Checking user's password characters occurences and mixture
     password_characters_occurences_dict, alphanumerical_characters_list = get_password_characters_occurences(user_password)
     check_password_characters_mixture(password_characters_occurences_dict, alphanumerical_characters_list)
         
-    #Get the password entropy
-    printc("[cyan3 bold underline][+] Password entropie[/cyan3 bold underline]\n")
-    password_entropy = get_entropy(len(user_password), password_characters_occurences_dict)
-    printc(password_level_sensibility(password_entropy))
-
-    #Try to crack user password using custom password dictionary or rockyou by default
-    printc("[cyan3 bold underline][+] Brute force attempt[/cyan3 bold underline]\n")
-    crack_password(user_password, passwords_dictionary_file_path) if user_response else crack_password(user_password)
+    # Getting the password entropy
+    printc("[cyan3 bold underline][*] Password entropy[/cyan3 bold underline]\n")
+    password_entropie = get_entropy(len(user_password), password_characters_occurences_dict)
+    printc(password_level_sensibility(password_entropie))
     
-    #Ask the user if they wanna generate a password
-    printc("\n[cyan3 bold underline][+] Password Generator[/cyan3 bold underline]\n")
-    user_response = input('Do you wanna generate a password using the password generator[yay/Nay] : ')
-    if user_response.lower() in ['yay', 'yes', 'y']:
-        while True:
-            password_length = int(input('Enter the password length: '))
-            if password_length >= 20:
-                break
-            print('The password should have 20 characters minimum!')
-        print(f"Generated password : {password_generator(password_length)}")
-    print("────────────────────────────────────────────────────────────────\n")
-    print('Feel free to hit me up for any suggestions(^_-)')
-    time.sleep(2)
-    print("Hmmm last thing: if your password was considered as weak, do not forget to CHANGE it ASAP(^.^)")
+    
+    # Asking the user if they want to generate a password
+    printc("\n[cyan3 bold underline][*] Password Generator[/cyan3 bold underline]\n")
+    try:
+        user_response = input('Do you wanna generate a password using our password generator[Yay/nay]: ')
+        if user_response.lower() in ['','yay', 'yes', 'y', 'yeah', 'yep']:
+            while True:
+                password_length = int(input('Enter the password length: '))
+                if password_length >= 20:
+                    break
+                printc('The password should have [red b]20[/red b] characters minimum!')
+            printc(f"Generated password: [magenta2 b]{password_generator(password_length)}[/magenta2 b]\n")
+    except KeyboardInterrupt:
+        sys.exit(print("\nGood Bye!"))
+    print("────────────────────────────────────────────────────────────────────────────")
+    printc("[yellow1 b][!][/yellow1 b] Don't forget to CHANGE your password [red b]ASAP[/red b] if it was considered unsecure.")
